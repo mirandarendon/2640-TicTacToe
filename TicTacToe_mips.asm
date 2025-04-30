39c73f2c1b02505099fef88df3af7e5fdeeaae98
Your Name <you@example.com>
2025-04-30 13:06:21 -0700

    Added main label and ran a macro

--- TicTacToe_mips.asm
+++ TicTacToe_mips.asm
@@ -1,9 +1,14 @@
-#Authors:
+#Names: Eulalia Pedro Andres, Marlenne Salcido, Miranda Rendon
 #2640-12PM
 
-include "macros.asm"
+.include "macros.asm"
 
 .data
 	row: .asciiz "----------------------------------------------------------"
 
 .text
+	main:
+		printString(row)
+		
+		li $v0, 10
+		syscall
