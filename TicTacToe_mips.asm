78dad5c4d379bffa2ea7013de56bc5fcf785a13a
Your Name <you@example.com>
2025-04-30 13:21:55 -0700

    Prints intro string

--- TicTacToe_mips.asm
+++ TicTacToe_mips.asm
@@ -4,11 +4,12 @@
 .include "macros.asm"
 
 .data
+	modePrompt: .asciiz "Welcome to Tic Tac Toe!\n\nWould you like to play (1) CPU Mode or (2) Two Player Mode: "
 	row: .asciiz "----------------------------------------------------------"
 
 .text
 	main:
-		printString(row)
+		printString(modePrompt)
 		
 		li $v0, 10
 		syscall
