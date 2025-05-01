ac88cf3dd9f4e065f23a62a1fbe0f5a99d588524
Your Name <you@example.com>
2025-04-30 19:33:48 -0700

    Added more prompts and asks for input on moves from user

--- TicTacToe_mips.asm
+++ TicTacToe_mips.asm
@@ -6,6 +6,17 @@
 .data
 intro: .asciiz "Welcome to Tic Tac Toe!"
 modePrompt: .asciiz "\n\nWould you like to play (1) One Player or (2) Two Player: "
+onePlayerStartPrompt: .asciiz "First move goes to (1) You or (2) CPU: "
+twoPlayersStartPrompt: .asciiz "First move goes to (1) Player 1 or (2) Player 2: "
+userTurnPrompt: .asciiz "\n---Your Turn---\n"
+CPUTurnPrompt: .asciiz "\n---CPU's Turn---\n"
+Player1TurnPrompt: .asciiz "\n---Player 1's Turn---\n"
+Player2TurnPrompt: .asciiz "\n---Player 2's Turn---\n"
+userWins: .asciiz "You win!"
+userLoses: .asciiz "You lose!"
+player1Wins: "Player 1 wins!"
+player2Wins: "Player 2 wins!"
+moveChoice: "Which square would you like to mark? Please enter a value from 1-9: "
 row: .asciiz "----------------------------------------------------------"
 anotherRound: .asciiz "\n\nWould you like to play again? (1) Yes or (2) No: "
 reprompt: .asciiz "That is not a valid input"
@@ -33,12 +44,34 @@ aGame:
 
 #game against computer	
 onePlay:
-
-
+printString(onePlayerStartPrompt)
+getInt
+move $t3, $v0 #$t3 stores who gets the first move
+#logic is currently not accounting for who starts. Might not ask in final product
+printString(userTurnPrompt)
+printString(moveChoice)
+getInt
+move $t4, $v0 #$t4 store where user wants to mark
+j replay
 #game with two users
 twoPlay:
-	
+printString(twoPlayersStartPrompt)
+getInt
+move $t3, $v0 #$t3 stores who gets the first move
+#logic is currently not accounting for who starts. Might not ask in final product
 
+#Player 1 Turn
+printString(Player1TurnPrompt)
+printString(moveChoice)
+getInt
+move $t4, $v0 #$t4 store where Player 1 wants to mark
+#Player 2 Turn
+printString(Player2TurnPrompt)
+printString(moveChoice)
+getInt
+move $t5, $v0 #$t5 store where Player 2 wants to mark
+
+j replay
 
 #chack if user wants to play again			
 replay:		
