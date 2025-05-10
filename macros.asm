d39a3122a7fead5db5e3cdc2584ba6ada6d7cb44
Your Name <you@example.com>
2025-05-10 11:27:44 -0700

    Adds macro that prints out empty board to bitmap display

--- macros.asm
+++ macros.asm
@@ -2,6 +2,8 @@
 #macros
 
 
+
+
 .macro printString(%string)
 	li $v0, 4
 	la $a0, %string
@@ -32,6 +34,45 @@
 	printString(row2)
 	printString(line)
 	printString(row3)
+.end_macro 
+	
+.macro ogBoardBitmap
+
+	add $s2, $zero, 1
+	la $s0, 0x10010000
+	li $s1, BLUE
+	li $s4, LIBLUE
+
+
+	rows:
+	sw $s1, 16($s0)
+	addi $s0, $s0, 4
+	sw $s1, 16($s0)
+	addi $s0, $s0, 20
+	sw $s1, 16($s0)
+	addi $s0, $s0, 4
+	sw $s1, 16($s0)
+	add $s2, $s2, 1
+	beq $s2, 17, readjust
+	addi $s0, $s0, 36
+	j rows
+	readjust:
+	subi $s0, $s0, 988
+	add $s2, $zero, 1
+	cols:
+	sw $s1, 256($s0)
+	addi $s0, $s0, 64
+	sw $s1, 256($s0)
+	addi $s0, $s0, 320
+	sw $s1, 256($s0)
+	addi $s0, $s0, 64
+	sw $s1, 256($s0)
+	add $s2, $s2, 1
+	beq $s2, 17, done
+	subi $s0, $s0, 444
+	j cols
+	done:
+	
 	
 .end_macro
 	
